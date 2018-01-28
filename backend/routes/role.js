236b499c9170250d231a263e87f7380ba6fef58b
Laura Zhukas <laura@lauras-air-4.wireless.concordia.ca>
2018-01-27 21:17:24 -0500

    Update rest api middleware

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -18,155 +18,144 @@ module.exports = function(app, passport) {
 
   })
 
-app.post("/_requestAccess", function(req, res) {
-  console.log("Here")
-  req.checkBody('patient', 'patient must not be empty.').notEmpty();
+  app.post("/_requestAccess", function(req, res) {
+    req.checkBody('patient', 'patient must not be empty.').notEmpty();
 
-  if(req.body['doctor'] == undefined){
-    req.checkBody('institution', 'institution must not be empty.').notEmpty();
-    }
-    else if(req.body['institution'] == undefined){
-    req.checkBody('doctor', 'doctor must not be empty.').notEmpty()
-    }
-  let errors = req.validationErrors();
-  if (errors){
-    res.status(400).json({
-      error: errors
+    if(req.body['doctor'] == undefined){
+      req.checkBody('institution', 'institution must not be empty.').notEmpty();
+      }
+      else if(req.body['institution'] == undefined){
+      req.checkBody('doctor', 'doctor must not be empty.').notEmpty()
+      }
+    let errors = req.validationErrors();
+    if (errors){
+      res.status(400).json({
+        error: errors
+      })
+    } 
+    let _Request = {
+      "$class": namespace + "Request",
+      "patient" : req.body['patient'] , 
+      "doctor": req.body['doctor'] , 
+      "institution": req.body['institution'] ,
+      
+    };
+
+    console.log("in")
+    assetchain.requestAccess(_Request).then((x)=> {
+      console.log("GOOD:=>\n",x) // Return OK response
+      res.json({
+        message: "added " + req.body['patient'],
+        added: true
+      })
+    }).catch((error) => {
+      console.log("BAD:=>\n", error) // Return error response
+      res.status(400).json({
+        message: "Not added",
+        added: false
+      })
     })
-  } 
-  let _Request = {
-    "$class": namespace + "Request",
-    "patient" : req.body['patient'] , 
-    "doctor": req.body['doctor'] , 
-    "institution": req.body['institution'] ,
+  });
+
+  app.post("/_ApproveReject", function(req, res) {
+    console.log("Here")
+    req.checkBody('record', 'record must not be empty.').notEmpty();
+    req.checkBody('userApproving', 'userApproving must not be empty.').notEmpty();
+    req.checkBody('approved', 'approved must not be empty.').notEmpty();
+    let errors = req.validationErrors();
     
-  };
-
-  console.log("in")
-  assetchain.requestAccess(_Request).then((x)=> {
-    console.log("GOOD:=>\n",x) // Return OK response
-    res.json({
-      message: "added " + req.body['patient'],
-      added: true
+    if (errors){
+      res.status(400).json({
+        error: errors
+      })
+    } 
+    let _ApproveReject = {
+      "$class": namespace + "ApproveReject",
+      "record": req.body['record'] , 
+      "userApproving": req.body['userApproving'] , 
+      "approved": req.body['approved'] , 
+      
+      
+    };
+    console.log("2")
+    assetchain.approveReject(_ApproveReject).then((x)=> {
+      console.log("GOOD:=>\n",x) // Return OK response
+      res.json({
+        message: "added " + req.body['record'].join(" "),
+        added: true
+      })
+    }).catch((error) => {
+      console.log("BAD:=>\n", error) // Return error response
+      res.status(400).json({
+        message: "Not added",
+        added: false
+      })
     })
-  }).catch((error) => {
-    console.log("BAD:=>\n", error) // Return error response
-    res.status(400).json({
-      message: "Not added",
-      added: false
-    })
-  })
-});
-
-app.get("/_ApproveReject", function(req, res) {
-
-  if(req.isAuthenticated() || process.env.TEST_MODE == 1){
-        req.checkBody('record', 'record must not be empty.').notEmpty();
-        req.checkBody('userApproving', 'userApproving must not be empty.').notEmpty();
-        req.checkBody('approved', 'approved must not be empty.').notEmpty();
-        let errors = req.validationErrors();
-        
-        if (errors){
-          res.status(400).json({
-            error: errors
-          })
-        } 
-        let _ApproveReject = {
-          "$class": namespace + "ApproveReject",
-          "record": req.body['record'] , 
-          "userApproving": req.body['userApproving'] , 
-          "approved": req.body['approved'] , 
-          
-          
-        };
-        assetchain.assignRole(_ApproveReject).then((x)=> {
-          console.log("GOOD:=>\n",x) // Return OK response
-          res.json({
-            message: "added " + req.body['record'].join(" "),
-            added: true
-          })
-        }).catch((error) => {
-          console.log("BAD:=>\n", error) // Return error response
-          res.status(400).json({
-            message: "Not added",
-            added: false
-          })
-        })
-  
-  }
   });
 
   app.get("/_GrantAccess", function(req, res) {
-
-    if(req.isAuthenticated() || process.env.TEST_MODE == 1){
-          req.checkBody('record', 'record must not be empty.').notEmpty();
-          req.checkBody('doctorGranting', 'doctorGranting must not be empty.').notEmpty();
-          let errors = req.validationErrors();
-          
-          if (errors){
-            res.status(400).json({
-              error: errors
-            })
-          } 
-          let _GrantAccess = {
-            "$class": namespace + "GrantAccess",
-            "record": req.body['record'] , 
-            "doctorGranting": req.body['doctorGranting'] , 
-            
-            
-          };
-          assetchain.assignRole(_GrantAccess).then((x)=> {
-            console.log("GOOD:=>\n",x) // Return OK response
-            res.json({
-              message: "added " + req.body['record'].join(" "),
-              added: true
-            })
-          }).catch((error) => {
-            console.log("BAD:=>\n", error) // Return error response
-            res.status(400).json({
-              message: "Not added",
-              added: false
-            })
-          })
+    req.checkBody('record', 'record must not be empty.').notEmpty();
+    req.checkBody('doctorGranting', 'doctorGranting must not be empty.').notEmpty();
+    let errors = req.validationErrors();
     
-    }
-    });
-
-
-    app.get("/_View", function(req, res) {
-
-      if(req.isAuthenticated() || process.env.TEST_MODE == 1){
-            req.checkBody('user', 'user must not be empty.').notEmpty();
-            req.checkBody('recordId', 'recordId must not be empty.').notEmpty();
-            let errors = req.validationErrors();
-            
-            if (errors){
-              res.status(400).json({
-                error: errors
-              })
-            } 
-            let _GrantAccess = {
-              "$class": namespace + "View",
-              "user": req.body['user'] , 
-              "recordId": req.body['recordId'] , 
-              
-             
-            };
-            assetchain.assignRole(_View).then((x)=> {
-              console.log("GOOD:=>\n",x) // Return OK response
-              res.json({
-                message: "added " + req.body['user'].join(" "),
-                added: true
-              })
-            }).catch((error) => {
-              console.log("BAD:=>\n", error) // Return error response
-              res.status(400).json({
-                message: "Not added",
-                added: false
-              })
-            })
+    if (errors){
+      res.status(400).json({
+        error: errors
+      })
+    } 
+    let _GrantAccess = {
+      "$class": namespace + "GrantAccess",
+      "record": req.body['record'] , 
+      "doctorGranting": req.body['doctorGranting'] , 
       
-      }
-      });
-    }
+      
+    };
+    assetchain.assignRole(_GrantAccess).then((x)=> {
+      console.log("GOOD:=>\n",x) // Return OK response
+      res.json({
+        message: "added " + req.body['record'].join(" "),
+        added: true
+      })
+    }).catch((error) => {
+      console.log("BAD:=>\n", error) // Return error response
+      res.status(400).json({
+        message: "Not added",
+        added: false
+      })
+    })
+  });
+
+  app.get("/_View", function(req, res) {
+    req.checkBody('user', 'user must not be empty.').notEmpty();
+    req.checkBody('recordId', 'recordId must not be empty.').notEmpty();
+    let errors = req.validationErrors();
+    
+    if (errors){
+      res.status(400).json({
+        error: errors
+      })
+    } 
+    let _GrantAccess = {
+      "$class": namespace + "View",
+      "user": req.body['user'] , 
+      "recordId": req.body['recordId'] , 
+      
+      
+    };
+    assetchain.assignRole(_View).then((x)=> {
+      console.log("GOOD:=>\n",x) // Return OK response
+      res.json({
+        message: "added " + req.body['user'].join(" "),
+        added: true
+      })
+    }).catch((error) => {
+      console.log("BAD:=>\n", error) // Return error response
+      res.status(400).json({
+        message: "Not added",
+        added: false
+      })
+    })
+
+  });
+}
 //-------------------------------------------------------------------------------------------------------
