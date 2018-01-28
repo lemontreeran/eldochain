f0efab828e328395b4f034eb0bf2c0e37561fbd9
Vishvajit Kher <vishvajit79@gmail.com>
2018-01-28 04:08:52 -0500

    transacrtion and historian is now updated on the page

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -4,6 +4,7 @@ guestControllers.controller('ListController', ['$rootScope','$scope', '$http', '
 function($rootScope, $scope, $http, $timeout, $location) {
 	// $rootScope.userId = $scope.userId || 'vishvajit79@gmail.com';
     $rootScope.userId = $scope.userId;
+    $rootScope.userId = 'vishvajit79@gmail.com';
     $rootScope.userLogin = function() {
         $rootScope.userId = $scope.userId;
         userId = $scope.userId;
@@ -62,4 +63,129 @@ function($rootScope, $scope, $http, $timeout, $location) {
         });
 		console.log(data);
     };
+
+    $rootScope.userDetails = function() {
+        var data = { 'doctorId' : $rootScope.userId};
+        $scope.requesting = true;
+        $http({
+            url: 'http://54.209.93.68:30001/api/Doctor/' + data['doctorId'],
+            method: "GET"
+        })
+		.then(function(response) {
+				console.log(response.data);
+				$scope.approvals = response.data.approvals;
+				$scope.json = response.data;
+				$scope.requesting = false;
+
+		},
+		function(response) { // optional
+			console.log(response.data);
+			$scope.requesting = false;
+			alert('There was an error while submitting request.');
+		});
+        console.log(data);
+    };
+    $scope.userDetails();
+    $rootScope.patientRecord = function($id) {
+        $scope.requesting = true;
+        $http({
+            url: 'http://54.209.93.68:30001/api/Record/' + $id,
+            method: "GET"
+        })
+            .then(function(response) {
+                    console.log(response.data);
+                    $scope.json = response.data;
+                    $scope.requesting = false;
+
+                },
+                function(response) { // optional
+                    console.log(response.data);
+                    $scope.requesting = false;
+                    alert('There was an error while submitting request.');
+                });
+    };
+
+    $rootScope.approvePatient = function($record,$value) {
+        var data = { 'record' : $record, 'approved': $value, 'userApproving': $rootScope.userId};
+        $scope.requesting = true;
+        $http({
+            url: 'http://54.209.93.68:3000/_approveReject',
+            method: "POST",
+			data: data
+        })
+            .then(function(response) {
+                    console.log(response.data);
+                    $scope.json = response.data;
+                    $scope.requesting = false;
+
+                },
+                function(response) { // optional
+                    console.log(response.data);
+                    $scope.requesting = false;
+                    alert('There was an error while submitting request.');
+                });
+    };
+
+    $rootScope.approvePatientByDoctor = function($record,$value) {
+        var data = { 'record' : $record, 'granted': $value};
+        $scope.requesting = true;
+        $http({
+            url: 'http://54.209.93.68:3000/_grantAccess',
+            method: "POST",
+            data: data
+        })
+            .then(function(response) {
+                    console.log(response.data);
+                    $scope.json = response.data;
+                    $scope.requesting = false;
+
+                },
+                function(response) { // optional
+                    console.log(response.data);
+                    $scope.requesting = false;
+                    alert('There was an error while submitting request.');
+                });
+    };
+
+    $rootScope.showDocument = function () {
+    	var data = {'doctorId': $rootScope.userId, 'recordsId': $rootScope.userId}
+        $scope.requesting = true;
+        $http({
+            url: 'http://54.209.93.68:3000/_view',
+            method: "POST",
+            data: data
+        })
+            .then(function(response) {
+                    console.log(response.data);
+                    $scope.json = response.data;
+                    $scope.requesting = false;
+
+                },
+                function(response) { // optional
+                    console.log(response.data);
+                    $scope.requesting = false;
+                    alert('There was an error while submitting request.');
+                });
+    };
+    $rootScope.showDocument();
+
+    $rootScope.historian = function () {
+        $scope.requesting = true;
+        $http({
+            url: 'http://54.209.93.68:30001/api/system/historian',
+            method: "GET"
+        })
+            .then(function(response) {
+                    console.log(response.data);
+                    $scope.historian = response.data;
+                    $scope.requesting = false;
+
+                },
+                function(response) { // optional
+                    console.log(response.data);
+                    $scope.requesting = false;
+                    alert('There was an error while submitting request.');
+                });
+    };
+    $rootScope.historian();
 }]);
